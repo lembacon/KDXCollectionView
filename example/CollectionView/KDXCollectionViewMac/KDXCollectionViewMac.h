592c1f06df5af1322824e04c850f20ebc8c6ace8
Daniel Martín <danimartinprieto@gmail.com>
2015-07-02 20:00:15 +0200

    Fixing problem with Carthage.

---
+++ example/CollectionView/KDXCollectionViewMac/KDXCollectionViewMac.h
@@ -0,0 +1,21 @@
+//
+//  KDXCollectionViewMac.h
+//  KDXCollectionViewMac
+//
+//  Created by Daniel Martín Prieto on 02/07/15.
+//  Copyright (c) 2015 Chongyu Zhu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KDXCollectionViewMac.
+FOUNDATION_EXPORT double KDXCollectionViewMacVersionNumber;
+
+//! Project version string for KDXCollectionViewMac.
+FOUNDATION_EXPORT const unsigned char KDXCollectionViewMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KDXCollectionViewMac/PublicHeader.h>
+#import <KDXCollectionViewMac/KDXCollectionView.h>
+#import <KDXCollectionViewMac/KDXCollectionViewCell.h>
+
+
--- example/CollectionView/KDXCollectionViewMacTests/KDXCollectionViewMacTests.swift
+++ example/CollectionView/KDXCollectionViewMacTests/KDXCollectionViewMacTests.swift
@@ -1,6 +1,6 @@
 //
-//  KDXCollectionViewTests.swift
-//  KDXCollectionViewTests
+//  KDXCollectionViewMacTests.swift
+//  KDXCollectionViewMacTests
 //
 //  Created by Daniel Martín Prieto on 02/07/15.
 //  Copyright (c) 2015 Chongyu Zhu. All rights reserved.
@@ -9,7 +9,7 @@
 import Cocoa
 import XCTest
 
-class KDXCollectionViewTests: XCTestCase {
+class KDXCollectionViewMacTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
